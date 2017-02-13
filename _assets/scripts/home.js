e5c4c05d8967f64e5384a5e86d1a0148bb44c4a1
Ashish Prajapati <prajapati.a@hashedin.com>
2017-02-13 12:01:37 +0530

    responsive navbar added

--- _assets/scripts/home.js
+++ _assets/scripts/home.js
@@ -1,1 +1,10 @@
 //Write JS logic here
+
+$(document).on('ready',function(){
+  $('.hamburger-button').on('click',function(){
+    $('.mobile-navbar').css('display','block');
+  })
+  $('.nav-close-button').on('click',function(){
+    $('.mobile-navbar').css('display','none');
+  })
+})
